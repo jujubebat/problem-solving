07eda47b400d2b0808cb1744e0681839e3fa184d
jujubebat <bbwwpark@naver.com>
2020-06-06 20:26:04 +0900

    문제풀이추가

--- VSproject/VSproject/방금그곡.cpp
+++ VSproject/VSproject/방금그곡.cpp
@@ -1,102 +1,107 @@
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
-#include<algorithm>
-#include<iostream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-typedef struct {
-	int time, order;
-}info;
+string convert(string str) {
+	string ret = "";
+	for (int i = 0; i < str.size(); i++) {
+		if (str[i] == '#') {
+			ret[ret.size() - 1] = tolower(ret[ret.size() - 1]);
+		}
+		else
+			ret += str[i];
+	}
+	return ret;
+}
 
-bool comp(info a, info b) {
-	if (a.time == b.time) //재생시간이 같으면
-		return a.order < b.order; //입력순서가 빠른순으로
-	else
-		return a.time > b.time; //재생시간이 다르면, 재생 시간이 긴순으로
+//cout << listen << " " << playNote.substr(j, listen.size()) << endl;
+
+bool comp(string listen, string playNote) {
+	for (int j = 0; j <= playNote.size() - listen.size(); j++) {
+		if (listen.compare(playNote.substr(j, listen.size())) == 0)
+			return true;
+	}
+	return false;
 }
 
 string solution(string m, vector<string> musicinfos) {
 	string answer = "";
+	int answerTime = 0;
 
-	vector<string> log;
-	vector<info> candi; // 재생시간, 입력순서
+	string listen = convert(m); // convert 문제 없음, m # 파싱
 
-	string tmp = "";
-	int idx = 0;
+	cout << "listen : " << listen << endl;
 
-	while (m.find("#", idx) != string::npos)
-	{
-		idx = m.find("#", idx);
-		char lower_alphabet = m[idx - 1] - 'A' + 'a';
-		string alphabet;
-		alphabet += lower_alphabet;
-		m.replace(idx - 1, 2, alphabet);
-	}
-
-	cout << m << endl;
-	for (int i = 0; i < musicinfos.size(); i++)
-	{
-		idx = 0;
-		while (musicinfos[i].find("#", idx) != string::npos)
-		{
-			idx = musicinfos[i].find("#", idx);
-			char lower_alphabet = musicinfos[i][idx - 1] - 'A' + 'a';
-			string alphabet;
-			alphabet += lower_alphabet;
-			musicinfos[i].replace(idx - 1, 2, alphabet);
-		}
-		cout << musicinfos[i] << endl;
-	}
-
-	for (int i = 0; i < musicinfos.size(); i++) {
-
-		for (int j = 0; j < musicinfos[i].size(); j++) {
-			if (musicinfos[i][j] == ',') musicinfos[i][j] = ' ';
-		}
-
-		istringstream iss(musicinfos[i]);
+	for (int i = 0; i < musicinfos.size(); i++) { // 음악들 for문돌리고
 		string st, et, title, note;
-
-		iss >> st; //시작시간
-		iss >> et; //끝나는시간
-		iss >> title; //제목
-		iss >> note; //악보
-
-		log.push_back(title);
-
-		int diff = ((et[0] - '0') * 10 + (et[1] - '0')) * 60 + ((et[3] - '0') * 10 + (et[4] - '0')) - ((st[0] - '0') * 10 + (st[1] - '0')) * 60 + ((st[3] - '0') * 10 + (st[4] - '0'));
-
-		string musicLog = "";
-		idx = 0;
-		while (diff--) {
-			musicLog += note[idx++];
-			if (idx == note.size())
-				idx = 0;
+		int cnt = 0;
+		for (int j = 0; j < musicinfos[i].size(); j++) { // , 을 공백으로 바꿔준다.
+			if (musicinfos[i][j] != ',' && cnt == 0)
+				st += musicinfos[i][j];
+			if (musicinfos[i][j] != ',' && cnt == 1)
+				et += musicinfos[i][j];
+			if (musicinfos[i][j] != ',' && cnt == 2)
+				title += musicinfos[i][j];
+			if (musicinfos[i][j] != ',' && cnt == 3)
+				note += musicinfos[i][j];
+			if (musicinfos[i][j] == ',')
+				cnt++;
 		}
 
-		for (int k = 0; k < musicLog.size(); k++) {
-			if (k + m.size() > musicLog.size()) break;
+		int start = ((st[0] - '0') * 10 + (st[1] - '0')) * 60 + ((st[3] - '0') * 10 + (st[4] - '0'));
+		int end = ((et[0] - '0') * 10 + (et[1] - '0')) * 60 + ((et[3] - '0') * 10 + (et[4] - '0'));
 
-			bool flag = true;
-			idx = 0;
+		if (end == 0) {
+			end = 12 * 60;
+		}
 
-			for (int j = k; j < k + m.size(); j++) {
-				if (m[idx++] != musicLog[j]) {
-					flag = false;
+		int playTime = end - start;
+		if (playTime < listen.size()) continue;
+
+		note = convert(note); // note #으로 변환 주어진 음악악보
+		string playNote = ""; // 실행된 음악 ( 시간고려)
+
+		int size = note.size(); // 주어진 음악악보 크기
+
+		for (int j = 0; j < playTime; j++) {
+			if (j > size - 1) { // 주어진 음악악보 크기보다 인덱스가 크면
+				playNote += note[j % size];
+			}
+			else {
+				playNote += note[j];
+			}
+		}
+
+		//if (playNote.find(listen) != string::npos) {
+		
+		if(comp(listen, playNote)){ // m파싱한값과, 시간고려한 음악악보를 비교
+			cout << "playNote : " << playNote << endl;
+			cout << "listen : " << listen << endl;
+			cout << "playTime : " << playTime << endl;
+
+			if (answer.size() == 0) {
+				answer = title;
+				answerTime = playTime;
+			}
+			else {
+				if (playTime > answerTime) {
+					answer = title;
+					answerTime = playTime;
 				}
 			}
-
-			if (flag) {
-				candi.push_back({ diff,i });
-			}
 		}
+		cout << endl;
 	}
 
-	if (candi.size() == 0) return "(None)";
+	if (answer.size() == 0)
+		return "(None)";
 
-	sort(candi.begin(), candi.end(), comp);
-	return log[candi[0].order];
+	return answer;
 }
+
+
 
