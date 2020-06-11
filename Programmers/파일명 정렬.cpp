5b774dc4ab0f65e82e97df1076460355e7b5ab66
jujubebat <bbwwpark@naver.com>
2020-06-12 01:44:56 +0900

    문제풀이추가

--- Programmers/파일명 정렬.cpp
+++ Programmers/파일명 정렬.cpp
@@ -4,14 +4,13 @@
 using namespace std;
 
 typedef struct {
-	string name; // 파일명
-	string head;  // HAED
-	int number; // NUMBER
+	string name;
+	string head;
+	int number;
 }fileData;
 
-// 정렬 기준 함수 (HEAD, NUMBER 순서로 정렬)
-bool comp(fileData a, fileData b) { 
-	if (a.head == b.head) { 
+bool comp(fileData a, fileData b) {
+	if (a.head == b.head) {
 		return a.number < b.number;
 	}
 	else
@@ -28,36 +27,29 @@ vector<string> solution(vector<string> files) {
 
 		for (int j = 0; j < files[i].size(); j++) {
 
-			if (isdigit(files[i][j])) { // 숫자라면, HEAD가 끝났다는 뜻.
-				idx = j; // 인덱스를 저장한다.
+			if (isdigit(files[i][j])) {
+				idx = j;
 				break;
 			}
-
-			// HEAD 추출 : 대소문자 구분을 하지 않으므로, 모두 소문자로 변환하여 추출.
-			head += tolower(files[i][j]); 
+			
+			head += tolower(files[i][j]);
 		}
 
 		for (int j = idx; j < files[i].size(); j++) {
 
-			// 숫자가 아니거나 5자리 이상이면, 중단.
 			if (!isdigit(files[i][j]) || number.size() >= 5) {
 				break;
 			}
 
-			// NUMBER 추출
-			number += files[i][j]; 
+			number += files[i][j];
 		}
-
-		// 파일명, HEAD, NUMBER을 구조체로 묶고, 구조체 배열에 넣는다.
 		fileDatas.push_back({ files[i], head, stoi(number) });
 	}
 	
-	// HEAD, NUMBER 순서로 stable sort를 수행한다.
 	stable_sort(fileDatas.begin(), fileDatas.end(), comp);
 
-	// 구조체에서 파일명만 추출하여 결과 벡터에 넣어준다.
 	for (int i = 0; i < fileDatas.size(); i++)
 		answer.push_back(fileDatas[i].name);
 
-	return answer; // 결과 반환.
+	return answer;
 }
